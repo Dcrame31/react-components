0b6043ad18bf4a23d033a258a5bb8e729704ff8c
Diane Crame <dcrame@icloud.com>
2020-11-20 17:23:35 -0800

    practice component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comments">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+      </div>
+      <a href="/" className="author">
+        <Samp></Samp>
+      </a>
+      <div className="metadata">
+        <span className="date">Today at 6:00PM</span>
+      </div>
+      <div className="text">Nice blog post!</div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
