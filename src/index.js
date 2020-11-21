0b895f140dd7536507095069cbe214302c6315bf
Diane Crame <dcrame@icloud.com>
2020-11-20 18:17:37 -0800

    created comment detail component

--- src/index.js
+++ src/index.js
@@ -19,6 +19,21 @@ const App = () => {
           <div className="text">Nice blog post!</div>
         </div>
       </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={"https://source.unsplash.com/random"} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
     </div>
   );
 };
