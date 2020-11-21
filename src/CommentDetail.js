7af90eeac3fee66ff021e7ce9345f7bd062f4d9b
Diane Crame <dcrame@icloud.com>
2020-11-20 19:01:12 -0800

    created author props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+  console.log(props);
   return (
     <div className="comment">
       <a href="/" className="avatar">
@@ -8,7 +9,7 @@ const CommentDetail = () => {
       </a>
       <div className="content">
         <a href="/" className="author">
-          Sam
+          {props.author}
         </a>
         <div className="metadata">
           <span className="date">Today at 6:00PM</span>
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail author="Sam" />
+      <CommentDetail author="Alex" />
+      <CommentDetail author="Jane" />
     </div>
   );
 };
