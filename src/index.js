98c2685a5acd184614d9366744cae836c5ca8e05
Diane Crame <dcrame@icloud.com>
2020-11-21 17:10:49 -0800

    nested CommentDetail component inside ApprovalCard

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 5:33PM"
-        text="WOW"
-        avatar="https://source.unsplash.com/random"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 3:00PM"
-        text="Great!"
-        avatar="https://source.unsplash.com/random"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 5:33PM"
+          text="WOW"
+          avatar="https://source.unsplash.com/random"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 3:00PM"
+          text="Great!"
+          avatar="https://source.unsplash.com/random"
+        />
+      </ApprovalCard>
+
       <CommentDetail
         author="Jane"
         timeAgo="Yesterday at 6:01AM"
